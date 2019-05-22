900818b7c525b1d5e9af6a15c2ce33b3d6b48580
justfoxing <justfoxingprojects@gmail.com>
2019-05-22 15:53:21 +1000

    Add initial interactive_mode support to ghidra_bridge

--- ghidra_bridge/ghidra_bridge.py
+++ ghidra_bridge/ghidra_bridge.py
@@ -1,3 +1,6 @@
+import sys
+import weakref
+
 from . import bridge
 
 """ Use this list to exclude modules and names loaded by the remote ghidra_bridge side from being loaded into our namespace.
@@ -5,24 +8,54 @@ This prevents the ghidra_bridge imported by ghidra_bridge_server being loaded ov
 You probably only want this for stuff imported by the ghidra_bridge_server script that might conflict on the local side (or which
 is totally unnecessary on the local side, like GhidraBridgeServer).
 """
-EXCLUDED_REMOTE_IMPORTS = ["logging", "subprocess", "ghidra_bridge", "GhidraBridgeServer"]
+EXCLUDED_REMOTE_IMPORTS = ["logging", "subprocess",
+                           "ghidra_bridge", "bridge", "GhidraBridgeServer"]
 
 GHIDRA_BRIDGE_NAMESPACE_TRACK = "__ghidra_bridge_namespace_track__"
 
 
+def find_ProgramPlugin(tool):
+    """ Use the provided tool (probably something like CodeBrowser) to find the 
+        PythonPlugin or GhidraScriptMgrPlugin that should be present (because the ghidra bridge server python script is running...)
+
+        These plugins extends ProgramPlugin, so they have access to useful state like
+        the current address, etc 
+    """
+    plugins = tool.getManagedPlugins()
+    plugin = None
+    for i in range(0, plugins.size()):
+        plugin = plugins.get(i)
+        if "PythonPlugin" == plugin.getName() or "GhidraScriptMgrPlugin" == plugin.getName():
+            return plugin
+
+    raise Exception(
+        "Couldn't find targeted plugins in {} from {}".format(plugins, tool))
+
+
 class GhidraBridge():
-    def __init__(self, server_host="127.0.0.1", server_port=0, connect_to_host=bridge.DEFAULT_HOST, connect_to_port=bridge.DEFAULT_SERVER_PORT, start_in_background=True, loglevel=None, namespace=None):
-        """ Set up a bridge. Default settings are for a client - connect to the default ghidra bridge server, set up a listening server on a random
-        port, and start it in a background thread. For a ghidra bridge server, specify the server_port, set connect_to_* to None and set
-        start_in_background to False
+    def __init__(self, connect_to_host=bridge.DEFAULT_HOST, connect_to_port=bridge.DEFAULT_SERVER_PORT, loglevel=None, namespace=None, interactive_mode=None):
+        """ Set up a bridge. Default settings connect to the default ghidra bridge server,
 
-        If namespace is specified (e.g., locals() or globals()), automatically calls get_flat_api() with that namespace. Note that this requires
-        connect_to_host and connect_to_port to not be None
+        If namespace is specified (e.g., locals() or globals()), automatically calls get_flat_api() with that namespace. 
+
+        loglevel for what logging messages you want to capture
+
+        interactive_mode should auto-detect interactive environments (e.g., ipython or not in a script), but 
+        you can force it to True or False if you need to. False is normal ghidra script behaviour 
+        (currentAddress/getState() etc locked to the values when the script started. True is closer to the 
+        behaviour in the Ghidra Jython shell - current*/getState() reflect the current values in the GUI
         """
-        self.bridge = bridge.Bridge(server_host=server_host, server_port=server_port,
-                                    connect_to_host=connect_to_host, connect_to_port=connect_to_port,
-                                    start_in_background=start_in_background, loglevel=loglevel)
+        self.bridge = bridge.BridgeClient(
+            connect_to_host=connect_to_host, connect_to_port=connect_to_port, loglevel=loglevel)
 
+        if interactive_mode is None:
+            # from https://stackoverflow.com/questions/2356399/tell-if-python-is-in-interactive-mode, sys.ps1 only present in interactive interpreters
+            interactive_mode = bool(getattr(sys, 'ps1', sys.flags.interactive))
+        self.interactive_mode = interactive_mode
+        self.interactive_listener = None
+
+        self.flat_api_modules_list = []
+        self.namespace_list = []
         self.namespace = None
         if namespace is not None:
             if connect_to_host is None or connect_to_port is None:
@@ -43,17 +76,108 @@ class GhidraBridge():
 
         remote_main = self.bridge.remote_import("__main__")
 
+        if self.interactive_mode:
+            # first, manually update all the current* values (this allows us to get the latest values, instead of what they were when the server started
+            tool = remote_main.state.getTool()  # note: tool shouldn't change
+            plugin = find_ProgramPlugin(tool)
+            locn = plugin.getProgramLocation()
+            # set the values as overrides in the bridged object - this prevents them from being changed in the remote object
+            remote_main._bridge_set_override(
+                "currentAddress", locn.getAddress())
+            remote_main._bridge_set_override(
+                "currentProgram", plugin.getCurrentProgram())
+            remote_main._bridge_set_override("currentLocation", locn)
+            remote_main._bridge_set_override(
+                "currentSelection", plugin.getProgramSelection())
+            remote_main._bridge_set_override(
+                "currentHighlight", plugin.getProgramHighlight())
+
+            # next, keep a reference to this module for updating these addresses
+            self.flat_api_modules_list.append(weakref.ref(remote_main))
+
+            # next, overwrite getState with the getState_fix
+            def getState_fix():
+                """ Used when in interactive mode - instead of calling the remote getState, 
+                    relies on the fact that the current* variables are being updated and creates
+                    a GhidraState based on them.
+
+                    This avoids resetting the GUI to the original values in the remote getState
+                """
+                return remote_main.ghidra.app.script.GhidraState(tool, tool.getProject(), remote_main.currentProgram, remote_main.currentLocation, remote_main.currentSelection, remote_main.currentHighlight)
+            remote_main._bridge_set_override("getState", getState_fix)
+
+            # finally, install a listener for updates from the GUI events
+            if self.interactive_listener is None:
+                def update_vars(currentProgram=None, currentLocation=None, currentSelection=None, currentHighlight=None):
+                    """ For all the namespaces and modules we've returned, update the current* variables that have changed
+                    """
+                    # clear out any dead references
+                    self.flat_api_modules_list = [
+                        module for module in self.flat_api_modules_list if module() is not None]
+
+                    update_list = [
+                        module() for module in self.flat_api_modules_list] + self.namespace_list
+                    for update in update_list:
+                        # possible that a module might have been removed between the clear out and preparing the update list
+                        if update is not None:
+                            if currentProgram is not None:
+                                update.currentProgram = currentProgram
+                            if currentLocation is not None:
+                                # match the order of updates in GhidraScript - location before address
+                                update.currentLocation = currentLocation
+                                update.currentAddress = currentLocation.getAddress()
+                            if currentSelection is not None:
+                                update.currentSelection = currentSelection if not currentSelection.isEmpty() else None
+                            if currentHighlight is not None:
+                                update.currentHighlight = currentHighlight if not currentHighlight.isEmpty() else None
+
+                # define the interactive listener here, because we need the remote ghidra object
+                class InteractiveListener(remote_main.ghidra.framework.model.ToolListener):
+                    def __init__(self, tool):
+                        self.tool = tool
+
+                        # register the listener against the remote tool
+                        tool.addToolListener(self)
+
+                    def stop_listening(self):
+                        # we're done, make sure we remove the tool listener
+                        self.tool.removeToolListener(self)
+
+                    def processToolEvent(self, plugin_event):
+                        """ Called by the ToolListener interface """
+                        event_name = plugin_event.getEventName()
+                        if "Location" in event_name:
+                            update_vars(currentProgram=plugin_event.getProgram(
+                            ), currentLocation=plugin_event.getLocation())
+                        elif "Selection" in event_name:
+                            update_vars(currentProgram=plugin_event.getProgram(
+                            ), currentSelection=plugin_event.getSelection())
+                        elif "Highlight" in event_name:
+                            update_vars(currentProgram=plugin_event.getProgram(
+                            ), currentHighlight=plugin_event.getHighlight())
+
+                self.interactive_listener = InteractiveListener(
+                    remote_main.state.getTool())
+
         if namespace is not None:
             # add a special var to the namespace to track what we add, so we can remove it easily later
             namespace[GHIDRA_BRIDGE_NAMESPACE_TRACK] = dict()
 
             # load in all the attrs from remote main, skipping the double underscores and avoiding overloading our own ghidra_bridge
-            for attr in remote_main._bridge_attrs:
-                if not attr.startswith("__") and attr not in EXCLUDED_REMOTE_IMPORTS:
-                    remote_attr = getattr(remote_main, attr)
-                    namespace[attr] = remote_attr
-                    # record what we added to the namespace
-                    namespace[GHIDRA_BRIDGE_NAMESPACE_TRACK][attr] = remote_attr
+            try:
+                for attr in remote_main._bridge_attrs + list(remote_main._bridge_overrides.keys()):
+                    if not attr.startswith("__") and attr not in EXCLUDED_REMOTE_IMPORTS:
+                        remote_attr = getattr(remote_main, attr)
+                        namespace[attr] = remote_attr
+                        # record what we added to the namespace
+                        namespace[GHIDRA_BRIDGE_NAMESPACE_TRACK][attr] = remote_attr
+            except Exception:
+                self.unload_flat_api(namespace)
+                raise
+
+            # if we're interactive, keep track of the namespace so we can update the current* values
+            if self.interactive_mode:
+                self.namespace_list.append(namespace)
 
         return remote_main
 
@@ -63,8 +187,12 @@ class GhidraBridge():
         """
         if namespace is None:
             if self.namespace is None:
-                raise Exception("Bridge wasn't initialized with a namespace - need to specify the namespace you want to unload from")
+                raise Exception(
+                    "Bridge wasn't initialized with a namespace - need to specify the namespace you want to unload from")
             namespace = self.namespace
+
+        if self.interactive_mode and namespace in self.namespace_list:
+            self.namespace_list.remove(namespace)
 
         if GHIDRA_BRIDGE_NAMESPACE_TRACK in namespace:
             for key, value in namespace[GHIDRA_BRIDGE_NAMESPACE_TRACK].items():
@@ -93,3 +221,6 @@ class GhidraBridge():
     def __exit__(self, type, value, traceback):
         if self.namespace is not None:
             self.unload_flat_api(self.namespace)
+            
+        if self.interactive_listener is not None:
+            self.interactive_listener.stop_listening()
